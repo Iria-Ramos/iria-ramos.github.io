48c7efb71944712f51f4abdb795f69eaa6313373
Iria Ramos <iriar.varela@gmail.com>
2025-09-03 19:11:06 +0200

    Starting Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://iria-ramos.github.io',
+    base: '/portafolio',
     vite: {    
         plugins: [tailwindcss()],  
     },
